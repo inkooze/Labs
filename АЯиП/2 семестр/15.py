9cd7178c3a0b65f3e26916e7b45a9d74f88b587a
inkooze <inkooza@gmail.com>
2026-03-10 21:54:05 +0300

    релиз

--- АЯиП/2 семестр/15.py
+++ АЯиП/2 семестр/15.py
@@ -1,5 +1,18 @@
 '''
-Работа №15 - Вариант  - Строки
-- 
+Работа №15 - Вариант 16 - Строки
+- В заданной последовательности символов заменить каждую последовательность из одинаковых символов длиной более трех на (k)s, где s – повторяющийся символ, k - число повторений.
+- При выполнении данного задания не использовать другие типы данных (множества, списки и др.) Также нельзя пользоваться функцией split. Т.е. необходимо реализовать классический алгоритм работы со строками.
 '''''
 
+string = 'aaabbbbbccbbbdddddddeeeeffghiiii'
+
+subString = string[0]
+for char in string + '\0':
+	if subString[0] == char:
+		subString += char
+	else:
+		if len(subString) > 3:
+			string = string.replace(subString, str(len(subString)) + subString[0], 1)
+		subString = char
+
+print(string)
