80874252eb09a92adbd996dd8240c1a4450e8b4a
inkooze <inkooza@gmail.com>
2026-03-24 21:08:38 +0300

    перепись 10 лабы

--- АЯиП/10.py
+++ АЯиП/10.py
@@ -1,38 +1,77 @@
 """
 Работа №10 - Вариант 16 - Матрицы (двумерные массивы данных)
 - Определить и поменять местами максимальное и минимальное значения двух квадратных матриц.
+- Переменные:
+	- a, b - матрицы;
+	- na, nb - кол-во строк-столбцов (строк = столбцов, так как матрицы квадратные) матриц a и b соответственно;
+	- temp - вспомогательная переменная для обмена;
+	- i, j - параметры циклов;
+	- amax, bmin - максимальный и минимальный элементы в матрицах a и b соответственно;
+	- aimax, ajmax - индексы строки и столбца максимального элемента в матрице a;
+	- bimin, bjmin - индексы строки и столбца минимального элемента в матрице b.
 """
 
-matrix1 = [
-	[1, 2],
-	[3, 4]
-]
+na = int(input('Введи кол-во строк-столбцов (т.к. матрица квадратная) в матрице a... '))
 
-matrix2 = [
-	[4, 3],
-	[2, 1]
-]
+a = [[0] * na for _ in range(na)]
+for i in range(na):
+	for j in range(na):
+		a[i][j] = int(input(f'a[{i}][{j}] = '))
 
-# Поиск максимума/минимума в матрице
-# minMaxFlag - True поиск максимума, False - минимума
-def matrixExtr(matrix: list[list], minMaxFlag: bool = False) -> tuple:
-	extVal, extValI, extValJ = matrix[0][0], 0, 0
+print('\nМатрица a:')
+for i in range(na):
+	for j in range(na):
+		print(a[i][j], end = '\t')
+	print()
 
-	for i, row in enumerate(matrix):
-		for j, el in enumerate(row):
-			if (minMaxFlag and el > extVal) or (not minMaxFlag and el < extVal):
-				extVal, extValI, extValJ = el, i, j
+nb = int(input('\nВведи кол-во строк-столбцов (т.к. матрица квадратная) в матрице b... '))
 
-		return extVal, extValI, extValJ
+b = [[0] * nb for _ in range(nb)]
+for i in range(nb):
+	for j in range(nb):
+		b[i][j] = int(input(f'b[{i}][{j}] = '))
 
-maxVal, maxValI, maxValJ = matrixExtr(matrix1, True)
-minVal, minValI, minValJ = matrixExtr(matrix2)
+print('\nМатрица b:')
+for i in range(nb):
+	for j in range(nb):
+		print(b[i][j], end = '\t')
+	print()
 
-matrix1[maxValI][maxValJ], matrix2[minValI][minValJ] = minVal, maxVal
+amax = a[0][0]
+aimax = 0
+ajmax = 0
+for i in range(na):
+	for j in range(na):
+		if a[i][j] > amax:
+			amax = a[i][j]
+			aimax = i
+			ajmax = j
 
-print(f'''- Максимальный элемент в первой матрице на строке { maxValI + 1 }, столбце { maxValJ + 1 } = { maxVal }
-- Минимальный элемент во второй матрице на строке { minValI + 1 }, столбце { minValJ + 1 } = { minVal }
+bmin = b[0][0]
+bimin = 0
+bjmin = 0
+for i in range(nb):
+	for j in range(nb):
+		if b[i][j] < bmin:
+			bmin = b[i][j]
+			bimin = i
+			bjmin = j
 
-Матрицы после перестановки максимального и минимального элементов местами:
-- Первая матрица: { matrix1 }
-- Вторая матрица: { matrix2 }''')
+print(f'\nМаксимальный элемент в матрице a: a[{aimax}][{ajmax}] = {amax}')
+print(f'Минимальный элемент в матрице b: b[{bimin}][{bjmin}] = {bmin}')
+
+temp = a[aimax][ajmax]
+a[aimax][ajmax] = b[bimin][bjmin]
+b[bimin][bjmin] = temp
+
+print('\nМатрица a после изменений:')
+for i in range(na):
+	for j in range(na):
+		print(a[i][j], end = '\t')
+	print()
+
+print('\nМатрица b после изменений:')
+for i in range(nb):
+	for j in range(nb):
+		print(b[i][j], end = '\t')
+	print()
